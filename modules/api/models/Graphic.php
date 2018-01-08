56ed0cbe7dab779e97f636abf90709edc8af7e49
rex <rex@dodcode.com>
2018-01-08 23:15:57 +0800

    api graphic

---
+++ modules/api/models/Graphic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rex
+ * Email: caoliang@simpleware.com.cn
+ * Date: 08/01/2018
+ * Time: 5:25 PM
+ */
+
+namespace app\modules\api\models;
+
+
+use yii\base\Model;
+
+class Graphic extends Model
+{
+
+}
